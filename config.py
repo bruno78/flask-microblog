378f516c7ecc99f17b0f5217e6f377dcb395af75
bruno78 <devbruno78@gmail.com>
2016-03-15 16:15:03 -0400

    Add Web Forms

---
+++ config.py
@@ -0,0 +1,10 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'http://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'http://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'http://www.myopenid.com'}
+]
